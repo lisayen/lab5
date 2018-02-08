3b785ce878bd3e2bf9a943b51b89911bf8ce49fc
lisayen <lisayen2005@gmail.com>
2018-02-08 10:12:32 -0800

    lab5

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,16 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
- }
+	console.log(data);
+	var name = req.query.name;
+	var description = req.query.description;
+	res.render("index", {
+		"friends": [
+		{
+			"name": neme,
+			"description": description,
+			"imageURL": "http://lorempixel.com/400/400/people"			
+		},
+	]
+});
+ };
